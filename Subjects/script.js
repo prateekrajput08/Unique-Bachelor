cd6d9b139e0119330fd13993ff7063bf047fb09b
Prateek Rajput <144830423+prateekrajput08@users.noreply.github.com>
2024-02-09 22:34:43 +0530

    Add files via upload

---
+++ Subjects/script.js
@@ -0,0 +1,21 @@
+/*========== FAQ ==========*/
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for(i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function (){
+        this.classList.toggle("active");
+
+        var pannel = this.nextElementSibling;
+
+        if(pannel.style.display === "block") {
+            pannel.style.display = "none";
+        } else {
+            pannel.style.display = "block";
+        }
+    });
+}
+
+
+
